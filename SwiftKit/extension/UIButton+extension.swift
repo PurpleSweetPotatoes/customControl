0ff6aa13847f81a7d58dce7eb05ff199584963b1
PurpleSweetPotatoes <purplesweetpottoes@gmail.com>
2017-06-09 23:08:06 +0800

    解决btn上图下字 文字显示不完全问题

--- SwiftKit/extension/UIButton+extension.swift
+++ SwiftKit/extension/UIButton+extension.swift
@@ -5,15 +5,8 @@
 //  Created by MrBai on 2017/5/16.
 //  Copyright © 2017年 baiqiang. All rights reserved.
 //
-
 import UIKit
 
-/// btnSubView location
-///
-/// - none: normal
-/// - left: image.origin.x == btn.origin.x, title behiand image
-/// - right: title.maxY == btn.maxY, title behiand image
-/// - imgTopTitleBottom: image(title).center.x = btn.center.x, image at top, title at bottom
 enum BtnSubViewAdjust {
     case none
     case left
@@ -39,6 +32,16 @@ extension UIButton {
         btn.setBordColor(color: UIColor.mainColor)
         return btn
     }
+    class func createCornerBtn(frame:CGRect, color:UIColor, title:String) -> UIButton {
+        let btn = UIButton(type: .custom)
+        btn.frame = frame
+        btn.setCorner(readius: btn.width * 0.5)
+        btn.setBordColor(color: color)
+        btn.setTitle(title, for: .normal)
+        btn.setTitleColor(color, for: .normal)
+        return btn
+    }
+    
     /// 调整btn视图和文字位置()
     ///
     /// - Parameters:
@@ -60,19 +63,23 @@ extension UIButton {
         var imageTop: CGFloat = 0
         var titleLeft: CGFloat = 0
         var titleTop: CGFloat = 0
+        var titleRift: CGFloat = 0
         if type == .imgTopTitleBottom {
             imageLeft = (width - imgView.frame.width) * 0.5 - imgView.frame.origin.x
             imageTop = spacing - imgView.frame.origin.y
-            titleLeft = (width - titleLab.frame.width) * 0.5 - titleLab.frame.origin.x
+            titleLeft = (width - titleLab.frame.width) * 0.5 - titleLab.frame.origin.x - titleLab.frame.origin.x
             titleTop = spacing * 2 + imgView.frame.height - titleLab.frame.origin.y
+            titleRift = -titleLeft - titleLab.frame.origin.x * 2
         }else if type == .left {
             imageLeft = spacing - imgView.frame.origin.x
             titleLeft = spacing * 2 + imgView.frame.width - titleLab.frame.origin.x
+            titleRift = -titleLeft
         }else {
             titleLeft = width - titleLab.frame.maxX - spacing
+            titleRift = -titleLeft
             imageLeft = width - imgView.right - spacing * 2 - titleLab.frame.width
         }
         self.imageEdgeInsets = UIEdgeInsetsMake(imageTop, imageLeft, -imageTop, -imageLeft)
-        self.titleEdgeInsets = UIEdgeInsetsMake(titleTop, titleLeft, -titleTop, -titleLeft)
+        self.titleEdgeInsets = UIEdgeInsetsMake(titleTop, titleLeft, -titleTop, titleRift)
     }
 }
